41ed3cc1f743e6a775c3a407b6eda4682c5bb3d2
Joshua Garnham <joshua.garnham@yahoo.co.uk>
2017-03-09 13:19:36 +0000

    Slim down code and add tasks

---
+++ README.md
@@ -0,0 +1,10 @@
+This little micro:bit demo lets you move a dot (ball) around the screen just by tilting the device.
+
+# Tasks
+
+- Make the movement less sensitive, consider adding a threshold before moving the dot
+- Stop the dot from rolling off the screen!
+- Make the dot move faster if you tilt the micro:bit more
+- Add another dot to act as a 'goal', when the dot gets to the goal then move it elsewhere
+- Add some internal score tracking so you get points every time you get to a goal  
+- Show the score & end the game when the user presses either of the buttoms
--- accelerate.py
+++ accelerate.py
@@ -8,31 +8,21 @@ image = Image("00000:"
 
 display.show(image)
 
-x_pos = 2 # origin top left
-y_pos = 2
-refresh_rate = 400
-
 while True:
-    sleep(refresh_rate)
-    
+    sleep(1000) # 1000 ms = 1 second
+
+    # x and y will vary between around -1000 to 1000
     x = accelerometer.get_x()
     y = accelerometer.get_y()
-            
-    if x > 150 and x_pos < 4:
-        image = image.shift_right(1)
-        x_pos = x_pos + 1
-    elif x < -150 and x_pos > 0:
-        image = image.shift_left(1)
-        x_pos = x_pos - 1
 
-    if y > 150 and y_pos < 4:
+    if x > 0 :
+        image = image.shift_right(1)
+    else:
+        image = image.shift_left(1)
+
+    if y > 0:
         image = image.shift_down(1)
-        y_pos = y_pos + 1
-    elif y < -150 and y_pos > 0:
+    else:
         image = image.shift_up(1)
-        y_pos = y_pos - 1
-        
-    biggest = max(abs(x), abs(y))            
-    refresh_rate = max(600 - biggest, 100)
 
     display.show(image)
